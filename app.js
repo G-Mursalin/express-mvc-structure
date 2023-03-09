9ba7f3dcf8a8a38f0b3897a82738beded8b2a80e
Golam Mursalin <golammursalin309@gmail.com>
2023-03-09 13:00:05 +0600

    project initialized

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const tourRoute = require("./routes/tourRoute");
 const userRoute = require("./routes/userRoute");
 const AppError = require("./utils/appError");
 const { globalErrorController } = require("./controllers/errorController");
+const cookieParser = require("cookie-parser");
 
 //Middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //Routs
 app.use("/api/v1/tours", tourRoute);
